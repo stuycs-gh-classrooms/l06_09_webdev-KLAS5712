c9db80ad374df523c10f0869efc5bb0707fc4943
Luca Jones <90658795+KLAS5712@users.noreply.github.com>
2022-06-02 23:16:52 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
 function setup() {
-  // put setup code here
+  createCanvas(800, 500); 
+  background(0);  
+  charSquares();
 }
 
-function draw() {
-  // put drawing code here
+function charToColor(c) {
+  var grayVal = c; 
+  var f = color(grayVal, grayVal, grayVal); 
+  return f; 
 }
+
+function drawChar(x, y, c) {
+  var i = c; 
+  square(x, y, i);
+}
+
+function charSquares() {
+  var x = 0; var y = 0; var c; 
+  
+  for (c = 'a'; c <= 'z'; c++) {
+    
+    var i = c;
+    
+    if (x > width) {
+      x = 0; y += i; 
+    } // end if statement 
+    
+    noStroke();
+    fill(charToColor(c));
+    drawChar(x, y, c);
+    x += c; 
+    
+  } // end for loop
+} // end charSquares
